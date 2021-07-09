10896d801beb02d98bbc89dade770feb04082e92
Railton Ramés <railtonrames@outlook.com>
2021-07-09 11:00:47 -0300

    Criando projeto ByteBank do curso de C# da Alura

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using ByteBank.Funcionarios;
+using ByteBank.Sistemas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalcularBonificacao(); 
+
+            UsarSistema();
+
+        }
+
+        public static void UsarSistema()
+        {
+            SistemaInterno sistemaInterno = new SistemaInterno();
+
+            Diretor francisco = new Diretor("000.000.000-02");
+            francisco.Nome = "Francisco";
+            francisco.Senha = "123";
+
+            GerenteDeConta iracema = new GerenteDeConta("000.000.000-04");
+            iracema.Nome = "Iracema";
+            iracema.Senha = "abc";
+
+            ParceiroComercial parceiro = new ParceiroComercial();
+            parceiro.Senha = "123456";
+
+            sistemaInterno.Logar(parceiro, "123456");
+
+            sistemaInterno.Logar(francisco, "123");
+            sistemaInterno.Logar(iracema, "abc");
+        }
+
+        public static void CalcularBonificacao()
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Funcionario nicolas = new Designer("000.000.000-01");
+            nicolas.Nome = "Nicolas";
+
+            Funcionario francisco = new Diretor("000.000.000-02");
+            francisco.Nome = "Francisco";
+
+            Funcionario clarice = new Auxiliar("000.000.000-03");
+            clarice.Nome = "Clarice";
+
+            Funcionario iracema = new GerenteDeConta("000.000.000-04");
+            iracema.Nome = "Iracema";
+
+            Desenvolvedor railton = new Desenvolvedor("000.000.000-05");
+            railton.Nome = "Railton";
+
+            gerenciadorBonificacao.Registrar(railton);
+            gerenciadorBonificacao.Registrar(iracema);
+            gerenciadorBonificacao.Registrar(clarice);
+            gerenciadorBonificacao.Registrar(francisco);
+            gerenciadorBonificacao.Registrar(nicolas);
+
+            Console.WriteLine("Total de bonificações do mês " +
+                gerenciadorBonificacao.GetTotalBonificacao());
+        }
+    }
+}
